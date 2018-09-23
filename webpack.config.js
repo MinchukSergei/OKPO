ed3cdffa9b8a5c82ef0279dfdd9d9bfc8e8f8a49
Sergey Minchuk <minchuk94@gmail.com>
2018-09-23 16:58:44 +0300

    Add chart example

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ var path = require('path');
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  devtool: 'source-map',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
