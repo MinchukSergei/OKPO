39d6e090fb0e5ff1b70f2d6446c50a89b803e892
Sergey Minchuk <minchuk94@gmail.com>
2018-10-07 20:44:22 +0300

    Add chart drawer. Add example

--- src/index.js
+++ src/index.js
@@ -1,22 +1,91 @@
 import Plotly from 'plotly.js-dist';
 
-let trace1 = {
-    x: [1, 2, 3, 4],
-    y: [10, 15, 13, 17],
-    type: 'scatter'
-};
+let fileButton = $('#choose-file-button');
+fileButton.bind('change', handleFile);
 
-let trace2 = {
-    x: [1, 2, 3, 4],
-    y: [16, 5, 11, 9],
-    type: 'scatter'
-};
+async function handleFile(evt) {
+    let file = evt.target.files[0];
 
-var layout = {
-    title: "Responsive to window's size!",
-    font: {size: 18}
-  };
+    if (!isJsonType(file)) {
+        alert('Type mismatch. Supports only *.json.');
+        return;
+    }
 
-let data = [trace1, trace2];
+    let chartData = await getChartData(file);
+    chartData = buildChartData(chartData);
+    drawChart(chartData);
+    evt.target.value = null;
+}
 
-Plotly.newPlot('root', data, layout, {responsive: true});
+function isJsonType(file) {
+    return file && file.type === 'application/json';
+}
+
+function getChartData(file) {
+    return new Promise((resolve) => {
+        let fr = new FileReader();
+
+        fr.onload = (e) => {
+            let fileData = e.target.result,
+                jsonData;
+
+            if (fileData) {
+                jsonData = JSON.parse(fileData);
+            }
+
+            resolve(jsonData);
+        };
+
+        fr.readAsBinaryString(file);        
+    });
+}
+
+function buildChartData(fileData) {
+    let traces = [], 
+        type = 'scatter', 
+        mode;
+    
+    if (fileData.type && fileData.type === 'markers') {
+        mode = 'markers';
+    } else if (fileData.type) {
+        type = fileData.type;
+    }
+
+    for (let tr of fileData.data) {
+        let trace = {
+            name: tr.name,
+            mode: mode,
+            type: type
+        };
+
+        let isPieChart = fileData.type && fileData.type === 'pie';
+
+        trace[isPieChart ? 'values' : 'x'] = tr.x;
+        trace[isPieChart ? 'labels' : 'y'] = tr.y;
+
+        traces.push(trace);
+    }
+
+    let layout = {
+        title: fileData.title,
+        showlegend: fileData.legend !== undefined ? fileData.legend : true,
+        font: {
+            size: fileData.fontSize || 18
+        },
+        xaxis: {
+            title: fileData.xAxis
+        },
+        yaxis: {
+            title: fileData.yAxis
+        }
+    };
+
+    return {
+        traces: traces,
+        layout: layout
+    };
+}
+
+function drawChart(data) {
+    Plotly.newPlot('chart', data.traces, data.layout, { responsive: true });
+}
